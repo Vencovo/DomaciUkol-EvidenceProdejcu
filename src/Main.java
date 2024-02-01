275506b3306e50214328d199bfa15f2b98b95124
Vaclav <Vaclav>
2024-02-01 22:33:04 +0100

    Blb

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,32 @@
-// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
-// then press Enter. You can now see whitespace characters in your code.
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
 public class Main {
     public static void main(String[] args) {
-        // Press Alt+Enter with your caret at the highlighted text to see how
-        // IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        String firstName = "Karel";
+        String secondName = "Vodicka";
+        LocalDate birthDate = LocalDate.of(1965, 12, 12);
+        int saleContracts = 23;
+        double saleCarrot = 183.87;
+        String liveCity = "Brno";
+        String carRegistrationPlate = "1B3-3456";
+        double carOilConsumption = 15.8;
+        String ipAdress = "192.168.68.01";
+        double averageYield = saleCarrot / saleContracts;
 
-        // Press Shift+F10 or click the green arrow button in the gutter to run the code.
-        for (int i = 1; i <= 5; i++) {
+        // Zaokrouhlení na dvě desetinná místa - zde jsem chvilku hledal a lámal si hlavu, jak zaokrouhlit čísla, nakonec jsem sahl pro pomoc chat GPT.
+        double roundedYield = Math.round(averageYield * 100.0) / 100.0;
 
-            // Press Shift+F9 to start debugging your code. We have set one breakpoint
-            // for you, but you can always add more by pressing Ctrl+F8.
-            System.out.println("i = " + i);
-        }
+        String message = "Průměrný výnos na smlouvu je: " + roundedYield + " Tun";
+
+        System.out.println(firstName + " " + secondName);
+        System.out.println(birthDate);
+        System.out.println("Bydliště: "+liveCity);
+        System.out.println(message);
+        System.out.println("SPZ:"+carRegistrationPlate+" a spotřeba vozu = "+carOilConsumption+"l/100km");
+
+
+
+
     }
 }
